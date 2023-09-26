f7d1b106f00e8848d8b88d29986dc4d9e487f07b
JAMES HARRADON <2harrj36@solent.ac.uk>
2023-09-26 12:26:24 +0100

    completed output task

---
+++ Week1/output_tasks.py
@@ -0,0 +1,32 @@
+# Display message to the console
+print("Welcome to COM411")
+print("In week 1 we will learn...")
+print()
+print("...How to use Git and GitHub")
+print("...How to output to the screen")
+print("...How to get user input")
+print()
+print("I hope you are enjoying the lesson thus far!")
+
+# Display escape characters
+print("\n Displays a new line")
+print("\t Displays a tab space")
+print("\\ Displays a back slash")
+print("\" Displays a double quote")
+print("\' Displays a single quote")
+
+print("\t\t\"I am programming!\"")
+
+# Display a box
+# Display a box
+print("##########")
+print("#        #")
+print("#        #")
+print("#        #")
+print("##########")
+print("    ##    ")
+print("##########")
+print("    ##    ")
+print("    ##    ")
+print("   ####   ")
+print("####  ####")
--- main.py
+++ main.py
@@ -2,4 +2,8 @@ print("Hi")
 
 name = "James"
 
+# this is called an f string
 print(f"Hi {name}")
+
+# can also do
+print("Hi", name)
