7feefc64f0f3b460fd6aaa41f84033157bfe8a69
JAMES HARRADON <2harrj36@solent.ac.uk>
2023-09-26 12:36:22 +0100

    Added code to read user input

---
+++ Week1/input_tasks.py
@@ -0,0 +1,8 @@
+# Ask user to enter their name
+print("What is your name?")
+name = input()
+print(f"It is nice to meet you {name}")
+
+# Read in user's name
+name = input("What is your name?")
+print(f"Bye {name}")
