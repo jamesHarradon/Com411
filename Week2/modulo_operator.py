0b47a29fed63fa0f139caf11a5b08266a5dda4ff
JAMES HARRADON <2harrj36@solent.ac.uk>
2023-10-03 11:46:04 +0100

    tested while loop

--- Week2/modulo_operator.py
+++ Week2/modulo_operator.py
@@ -1,8 +1,15 @@
-string_num = input("Please enter a whole number: ")
 
+# string_num = input("Please enter a whole number: ")
+# num = int(string_num)
+# while type(string_num) != type(int):
+#     string_num = input("Please enter a whole number: ")
+
+string_num = input("Please enter a whole number: ")
 num = int(string_num)
 
 if num % 2 == 0:
     print("The number is an even number")
 elif num % 2 != 0:
     print("The number is an odd number")
+else:
+    print("Please enter a valid whole number")
