4b50653a38f074dfc721eeb51683a7558ff7279f
JAMES HARRADON <2harrj36@solent.ac.uk>
2023-10-03 12:42:55 +0100

    Added code to demonstrate the use of multiple nested statements

--- Week2/nested_if.py
+++ Week2/nested_if.py
@@ -12,3 +12,33 @@ elif cover_type == "hard":
     print("Books with hard covers can be more expensive!")
 else:
     print("Please enter either hard or soft")
+
+
+# find your phone
+
+
+found_phone = False
+
+while found_phone == False:
+    where_to_look = input("Where should I look? ")
+    if where_to_look == "bedroom":
+        where_in_bedroom = input("Where should I look in the bedroom? ")
+        if where_in_bedroom == "under bed":
+            print("Found some shoes but no phone")
+        else:
+            print("Found some mess but no phone")
+    elif where_to_look == "bathroom":
+        where_in_bathroom = input("Where should I look in the bathroom? ")
+        if where_in_bathroom == "in bath":
+            print("Found a rubber duck but no phone")
+        else:
+            print("Found bathroom stuff but no phone")
+    elif where_to_look == "living room":
+        where_in_living_room = input("Where should I look in the living room? ")
+        if where_in_living_room == "on the table":
+            print("Yes! I found my phone!")
+            found_phone = True
+        else:
+            print("Found some stuff but no phone")
+    else:
+        print("I do not know where that is but I will keep looking...")
