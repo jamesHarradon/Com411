f88194556232dc1185b27cee11a255de4040f257
JAMES HARRADON <2harrj36@solent.ac.uk>
2023-10-03 12:05:43 +0100

    Added code to demonstrate how a variable can be used as a counter

--- Week2/comparison_operators.py
+++ Week2/comparison_operators.py
@@ -9,3 +9,33 @@ elif num1 < num2:
     print("The first number is the smallest!")
 else:
     print("Both numbers are equal!")
+
+
+num_string1 = input("Please input a number: ")
+num1 = int(num_string1)
+num_string2 = input("Please input a second number: ")
+num2 = int(num_string2)
+num_string3 = input("Please input a third number: ")
+num3 = int(num_string3)
+
+countOfOdd = 0
+countOfEven = 0
+
+if num1 % 2 == 0:
+    countOfEven += 1
+else:
+    countOfOdd += 1
+
+
+
+if num2 % 2 == 0:
+    countOfEven += 1
+else:
+    countOfOdd += 1
+
+if num3 % 2 == 0:
+    countOfEven += 1
+else:
+    countOfOdd += 1
+
+print(f"There were {countOfEven} even and {countOfOdd} odd numbers.")
