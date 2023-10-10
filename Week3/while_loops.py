99f66e34400dc61f10f19c6ca8ea50e7c247c1ab
JAMES HARRADON <2harrj36@solent.ac.uk>
2023-10-10 11:13:20 +0100

    Added code to demonstrate the use of a simple while loop

---
+++ Week3/while_loops.py
@@ -0,0 +1,8 @@
+noOfApplesToRemove = input("How many apples should I remove? ")
+noOfApplesToRemoveInt = int(noOfApplesToRemove)
+applesRemoved = 0
+
+while applesRemoved < noOfApplesToRemoveInt:
+    print("Removed apple.")
+    applesRemoved += 1
+
