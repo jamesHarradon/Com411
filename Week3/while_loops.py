de32d719923a5f14c97a5bd6571a070fb352b07a
JAMES HARRADON <2harrj36@solent.ac.uk>
2023-10-10 12:03:27 +0100

    Added code to demonstrate the use of summing with a while loop

--- Week3/while_loops.py
+++ Week3/while_loops.py
@@ -1,35 +1,65 @@
-noOfApplesToRemove = input("How many apples should I remove? ")
-noOfApplesToRemoveInt = int(noOfApplesToRemove)
-applesRemoved = 0
+# noOfApplesToRemove = input("How many apples should I remove? ")
+# noOfApplesToRemoveInt = int(noOfApplesToRemove)
+# applesRemoved = 0
+#
+# while applesRemoved < noOfApplesToRemoveInt:
+#     print("Removed apple.")
+#     applesRemoved += 1
+#
+# obsToAvoid = input("How many obstacles must I avoid? ")
+# obsToAvoidInt = int(obsToAvoid)
+#
+# noOfObs = 0
+#
+# while noOfObs < obsToAvoidInt:
+#     print("Avoiding...", end="")
+#     noOfObs += 1
+#     print(f"Done {noOfObs} obstacles avoided!")
+#
+#
+# barsToChangeInt = int(input("How many bars should be charged? "))
+# noOfBarsCharged = 0
+# bar = "█ "
+#
+# while noOfBarsCharged < barsToChangeInt:
+#     noOfBarsCharged += 1
+#     print("Charging:", end="")
+#     print(f"{noOfBarsCharged * bar}")
+# print("The battery is fully charged.")
+#
+# # len function - finds length of string
+#
+# phrase = input("Please enter a phrase: ")
+# print(phrase)
+# phraseLength = len(phrase)
+# while phraseLength > 0:
+#     print("Hi ", end="")
+#     phraseLength -= 1
 
-while applesRemoved < noOfApplesToRemoveInt:
-    print("Removed apple.")
-    applesRemoved += 1
+# Calculate the sum of the first 100 numbers
 
-obsToAvoid = input("How many obstacles must I avoid? ")
-obsToAvoidInt = int(obsToAvoid)
+print("Calculating the sum of the first 100 numbers...")
 
-noOfObs = 0
+num = 0
+total = 0
 
-while noOfObs < obsToAvoidInt:
-    print("Avoiding...", end="")
-    noOfObs += 1
-    print(f"Done {noOfObs} obstacles avoided!")
+while num < 101:
+    total += num
+    num += 1
+print(f"Done! The answer is {total}")
 
 
-barsToChangeInt = int(input("How many bars should be charged? "))
-noOfBarsCharged = 0
-bar = "█ "
 
-while noOfBarsCharged < barsToChangeInt:
-    noOfBarsCharged += 1
-    print("Charging:", end="")
-    print(f"{noOfBarsCharged * bar}")
-print("The battery is fully charged.")
 
-phrase = input("Please enter a phrase: ")
-print(phrase)
-phraseLength = len(phrase)
-while phraseLength > 0:
-    print("Hi ", end="")
-    phraseLength -= 1
+
+
+
+
+
+
+
+
+
+
+
+
