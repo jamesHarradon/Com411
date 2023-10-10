5d85444203ffe0501cdb8d2838339a530b9a2b1b
JAMES HARRADON <2harrj36@solent.ac.uk>
2023-10-10 12:45:05 +0100

    Added code to demonstrate the use of a simple for loop

--- Week3/for_loops.py
+++ Week3/for_loops.py
@@ -1,34 +1,43 @@
-numOfMountains = int(input("How many mountains should I display?"))
+# numOfMountains = int(input("How many mountains should I display?"))
+#
+# print("Displaying...")
+#
+# for count in range(numOfMountains):
+#     print("     __")
+#     print("    /  \_ ")
+#     print("   /^    \\")
+#     print("  /  ^    \_")
+#     print("_/ ^ ^     ^\\")
+#     print("/  ^     ^    \\")
+#
+# print("Done!")
+#
+# # Count down
+#
+#
+# howFar = int(input("How far are we from the target? "))
+# print(howFar)
+#
+# for count in range(howFar, 0, -1):
+#     print(f"{count} steps remaining")
+#
+# print("Target achieved!")
+#
+# # cheat way
+# howFar = int(input("How far are we from the target? "))
+# print(howFar)
+#
+# for count in range(howFar):
+#     print(f"{howFar - count} steps remaining")
+#
+# print("Target achieved!")
 
-print("Displaying...")
+# Range
 
-for count in range(numOfMountains):
-    print("     __")
-    print("    /  \_ ")
-    print("   /^    \\")
-    print("  /  ^    \_")
-    print("_/ ^ ^     ^\\")
-    print("/  ^     ^    \\")
+level = int(input("What level of brightness is required?"))
+print(level)
+x = "*"
 
-print("Done!")
-
-# Count down
-
-
-howFar = int(input("How far are we from the target? "))
-print(howFar)
-
-for count in range(howFar, 0, -1):
-    print(f"{count} steps remaining")
-
-print("Target achieved!")
-
-# cheat way
-howFar = int(input("How far are we from the target? "))
-print(howFar)
-
-for count in range(howFar):
-    print(f"{howFar - count} steps remaining")
-
-print("Target achieved!")
-
+print("Adjusting brightness...")
+for count in range(2, level, 2):
+    print(f"Brightness level: {x * count}")
