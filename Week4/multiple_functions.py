23b844a4f12bc8b80683d89dcce79d1daa033ed2
JAMES HARRADON <2harrj36@solent.ac.uk>
2023-10-17 12:45:10 +0100

    Added code to demonstrate the use of return values

--- Week4/multiple_functions.py
+++ Week4/multiple_functions.py
@@ -10,3 +10,23 @@ def create_ladder():
     display_ladder(steps)
 
 create_ladder()
+
+
+def sum_weights(charWeight, invWeight):
+    return charWeight + invWeight
+
+def calc_avg_weight(charWeight, invWeight):
+    return sum_weights(charWeight, invWeight) / 2
+
+def run():
+    charWeight = int(input("What is the character weight?"))
+    invWeight = int(input("What is the inventory weight?"))
+    whatFunc = input("Enter either sum or average")
+    if whatFunc == "sum":
+        sum_weights(charWeight, invWeight)
+    elif whatFunc == "average":
+        calc_avg_weight(charWeight, invWeight)
+    else:
+        print("Invalid input. Please enter either sum or average")
+
+run()
