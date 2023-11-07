0e24f2e8937a2e914be3bfa13b1d1cdb532fe36d
JAMES HARRADON <2harrj36@solent.ac.uk>
2023-11-07 12:34:51 +0000

    Added code to demonstrate how to manipulate and sort a set

--- Week7/set_tasks.py
+++ Week7/set_tasks.py
@@ -27,4 +27,44 @@ def run_task2():
         obs_set.add((value, obs_list.count(value)))
     print(obs_set)
 
-run_task2()
+#run_task2()
+
+
+def observed_items2():
+    observations = []
+    while len(observations) < 5:
+        observation = input("Please enter an observation:")
+        observations.append(observation)
+    return observations
+
+# below is how to do a do while loop in python
+def remove_observations(list_of_obs):
+    while True:
+        decision = input("Do you wish to remove an observation (yes/no) ?")
+        if decision == "no":
+            break
+        obs = input("Please type the name of the observation you wish to remove: ")
+        list_of_obs.remove(obs)
+        print("Done!")
+
+def run_task3():
+    obs = observed_items2()
+    remove_observations(obs)
+    sorted(obs)
+    obs_set = set()
+    for value in obs:
+        obs_set.add((value, obs.count(value)))
+    for value in obs_set:
+        print(f"{value[0]}")
+
+
+run_task3()
+
+
+
+
+
+
+
+
+
