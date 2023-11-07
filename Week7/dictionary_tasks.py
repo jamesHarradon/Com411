8581f69fb352f26e5ee67ea7e2b73dfba926b47b
JAMES HARRADON <2harrj36@solent.ac.uk>
2023-11-07 12:45:00 +0000

    Added code to demonstrate how to iterate through a dictionary

--- Week7/dictionary_tasks.py
+++ Week7/dictionary_tasks.py
@@ -6,3 +6,23 @@ def run():
     print(pattern())
 
 run()
+
+
+def display_keys(data):
+    for key in data.keys():
+        print(key)
+
+def display_values(data):
+    for value in data.values():
+        print(value)
+
+def display_items(data):
+    for item in data.items():
+        print(item)
+
+def run():
+    display_keys(pattern())
+    display_values(pattern())
+    display_items(pattern())
+
+run()
