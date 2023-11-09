870a96db296311ef9e6074add1c4b54e074bdcbc
James Harradon <james.harradon@gmail.com>
2023-11-09 19:50:26 +0000

    Added code to display a menu of options

--- Week6/Titanic/titanic_task.py
+++ Week6/Titanic/titanic_task.py
@@ -14,9 +14,19 @@ def load_data(file_path):
             records.append(values)
     print("Done!")
 
+def display_menu():
+    choice = input("Please select one of the following options:\n[1] Display all passenger names\n[2] Display all surviving passengers\n[3] Display number of passengers per gender\n[4] Display number of passengers per age group\n")
+    choice_int = int(choice)
+    return choice_int
+
 def run():
     load_data("titanic.csv")
     num_records = len(records)
     print(f"Successfully loaded {num_records} records.")
+    selected_option = display_menu()
+    print(f"You have selected option: {selected_option}")
 
 run()
+
+
+
