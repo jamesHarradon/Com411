f5a2ba2dfbdcb6fbcac1115c26425c2e6eda8fd9
James Harradon <james.harradon@gmail.com>
2023-11-09 19:58:31 +0000

    Added code to display names of passengers

--- Week6/Titanic/titanic_task.py
+++ Week6/Titanic/titanic_task.py
@@ -19,14 +19,23 @@ def display_menu():
     choice_int = int(choice)
     return choice_int
 
+def display_passenger_names():
+    print("The names of the passengers are: ")
+    for record in records:
+        passenger_name = record[3]
+        print(passenger_name)
+
 def run():
     load_data("titanic.csv")
     num_records = len(records)
     print(f"Successfully loaded {num_records} records.")
     selected_option = display_menu()
     print(f"You have selected option: {selected_option}")
+    if selected_option == 1: 
+        display_passenger_names()
+    else:
+        print("Error! Option not recognised")
 
 run()
-
 
 
