f4a9f5c4100db1b4a76363156e624622a9c11fb0
JAMES HARRADON <2harrj36@solent.ac.uk>
2023-11-14 12:41:56 +0000

    Added code to display single plot - need to complete multiple

--- Week8/plots.py
+++ Week8/plots.py
@@ -10,3 +10,13 @@ def run_task1():
     display_line(x_values, y_values)
 
 run_task1()
+
+def small():
+    x_values = [3, 3, 4, 4, 3]
+    y_values = [3, 4, 4, 3, 3]
+    plt.plot(x_values, y_values, "or--")
+    plt.show()
+
+small()
+
+def medium():
