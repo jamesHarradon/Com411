23ff5d003d1f8810cdbef6d58b0634f07b4602cf
JAMES HARRADON <2harrj36@solent.ac.uk>
2023-11-21 11:43:32 +0000

    Updated classes human and robot with methods to display objects

--- Week9/robot.py
+++ Week9/robot.py
@@ -9,6 +9,12 @@ class Robot:
     def display(self):
         print(f"I am {self.name}")
 
+    def __repr__(self):
+        return f"robot(name={self.name}, age={self.age})"
+
+    def __str__(self):
+        return f"Robot {self.name} is {self.age} years old."
+
 
 if __name__ == "__main__":
     robot = Robot()
